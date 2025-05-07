cedd5758bb2e75bed98e6e5ebef146d1fe9cc3d7
Serhii Tymoshenko <serhij.tymoschenko@gmail.com>
2025-05-07 22:26:22 +0200

    Added rca model

---
+++ data/entities/Rca.cs
@@ -0,0 +1,27 @@
+namespace ds_rca.data.entities;
+
+public class Rca
+{
+    public required string Title { get; init; }
+    public required string Description { get; init; }
+    public required string AuthorName { get; init; }
+    public required string Count { get; init; }
+    public required string Price { get; init; }
+    public required string ShopUrl { get; init; }
+    public required string ImageUrl { get; init; }
+    public required RcaTraits Traits { get; set; }
+}
+
+public class RcaTraits
+{
+    public required string Face { get; init; }
+    public required string Eyes { get; init; }
+    public required string Tops { get; init; }
+    public required string Bottoms { get; init; }
+    public required string Background { get; init; }
+    public required string Hair { get; init; }
+    public required string HairBack { get; init; }
+    public required string Hats { get; init; }
+    public required string Left { get; init; }
+    public required string Right { get; init; }
+}
