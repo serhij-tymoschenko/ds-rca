f63d1c99f792e8b638dbaef8e2326cd37d9f18e2
Serhii Tymoshenko <serhij.tymoschenko@gmail.com>
2025-05-08 22:01:52 +0200

    Added contract recheck

--- services/ContractService.cs
+++ services/ContractService.cs
@@ -9,11 +9,16 @@ public class ContractService(PolyscanApi api, RedditGqlApi gqlApi)
 {
     public async Task StartAsync()
     {
+        var previousEntityIds = new List<string>();
         while (true)
+        {
             try
             {
                 var lastId = await Database.GetLastEntityIdAsync();
                 var entityIds = await api.GetEntityIdsAsync();
+                entityIds = previousEntityIds
+                    .Concat(entityIds)
+                    .ToList();
                 if (entityIds == null) throw new Exception("No entityIds fetched");
 
                 if (lastId.Length > 0)
@@ -31,14 +36,16 @@ public class ContractService(PolyscanApi api, RedditGqlApi gqlApi)
                     entityIds.Reverse();
                     var storefrontIds = await gqlApi.GetStorefrontIdsAsync(token, entityIds);
                     if (storefrontIds == null) throw new Exception("No storefrontIds fetched");
-                    
+
+                    previousEntityIds = new List<string>();
                     var rcas = new List<Rca>();
-                    foreach (var id in storefrontIds)
+                    foreach (var idIndexed in storefrontIds.Select((id, index) => new {id, index}))
                     {
-                        var rca = await gqlApi.GetRcaAsync(token, id);
+                        var rca = await gqlApi.GetRcaAsync(token, idIndexed.id);
+                        if (rca == null) previousEntityIds.Add(entityIds[idIndexed.index]);
                         if (rca != null) rcas.Add((Rca)rca);
                     }
-
+                    
                     rcas.ForEach(rca =>
                     {
                         Bot.PostRcaAsync(rca, MessageType.CONTRACT);
@@ -53,10 +60,14 @@ public class ContractService(PolyscanApi api, RedditGqlApi gqlApi)
                 {
                     await Database.SetLastEntityIdAsync(entityIds[0]);
                 }
+
             }
             catch (Exception e)
             {
                 Console.WriteLine($"Error getting contracts: {e.Message}");
             }
+            
+            Thread.Sleep(2400);
+        }
     }
 }
