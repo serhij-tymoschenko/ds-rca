4f3aebf249446d64e49f365276a1a9323114b4be
Serhii Tymoshenko <serhij.tymoschenko@gmail.com>
2025-05-08 16:30:41 +0200

    Fixed regex

--- utils/constants/AccessoriesRegex.cs
+++ utils/constants/AccessoriesRegex.cs
@@ -2,12 +2,12 @@ namespace ds_rca.utils.constants;
 
 public class AccessoriesRegex
 {
-    public static string Right = "accessory_([^_]+)$";
-    public static string Left = "accessory_back_([^_]+)$";
-    public static string Bottoms = "body_bottom_([^_]+)$";
-    public static string Tops = "body_([^_]+)$";
-    public static string Face = "face_lower_([^_]+)$";
-    public static string Eyes = "face_upper_([^_]+)$";
-    public static string Hats = "head_accessory_([^_]+)$";
-    public static string Hair = "hair_([^_]{11})(?:_([^_]{11}))?";
+    public static string Right = "accessory_(.{11})$";
+    public static string Left = "accessory_back_(.{11})$";
+    public static string Bottoms = "body_bottom_(.{11})$";
+    public static string Tops = "body_(.{11})$";
+    public static string Face = "face_lower_(.{11})$";
+    public static string Eyes = "face_upper_(.{11})$";
+    public static string Hats = "head_accessory_(.{11})$";
+    public static string Hair = "hair_(.{11})(?:_(.{11}))?";
 }
