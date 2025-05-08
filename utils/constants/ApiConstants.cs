bdb154633bdba4f0dbeb241ea78c3c89c83ac94f
Serhii Tymoshenko <serhij.tymoschenko@gmail.com>
2025-05-08 23:09:42 +0200

    Fixed storefront regex

--- utils/constants/ApiConstants.cs
+++ utils/constants/ApiConstants.cs
@@ -13,5 +13,5 @@ public class ApiConstants
     public const string USER_AGENT = "Reddit/Version 2023.21.0/Build 956283/Android 13";
     public const string CLIENT_ID = "ohXpoqrZYub1kg";
     
-    public const string STOREFRONT = @"strorefront_nft_";
+    public const string STOREFRONT = @"data-outfit-id=""(.*?)""";
 }
