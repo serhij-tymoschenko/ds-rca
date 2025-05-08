00ff68fc687c6f73155af81597dcdae446689031
Serhii Tymoshenko <serhij.tymoschenko@gmail.com>
2025-05-08 21:26:34 +0200

    Services refactored

--- services/ContractService.cs
+++ services/ContractService.cs
@@ -1,5 +1,4 @@
 using ds_rca.bot;
-using ds_rca.config;
 using ds_rca.data.db;
 using ds_rca.data.entities;
 using ds_rca.data.remote.api;
@@ -11,67 +10,53 @@ public class ContractService(PolyscanApi api, RedditGqlApi gqlApi)
     public async Task StartAsync()
     {
         while (true)
-        {
             try
             {
                 var lastId = await Database.GetLastEntityIdAsync();
                 var entityIds = await api.GetEntityIdsAsync();
+                if (entityIds == null) throw new Exception("No entityIds fetched");
 
                 if (lastId.Length > 0)
                 {
-                    if (entityIds != null)
+                    var token = await gqlApi.GetTokenAsync();
+                    if (token == null) throw new Exception("Token not generated");
+
+                    var isContainsLastId = entityIds.Contains(lastId);
+                    if (isContainsLastId)
                     {
-                        var token = await gqlApi.GetTokenAsync();
-
-                        if (token != null)
-                        {
-                            var isContainsLastId = entityIds.Contains(lastId);
-
-                            if (isContainsLastId)
-                            {
-                                var lastIdIndex = entityIds.IndexOf(lastId);
-                                entityIds = entityIds.Slice(0, lastIdIndex);
-                            }
-
-                            entityIds.Reverse();
-
-                            var storefrontIds = await gqlApi.GetStorefrontIdsAsync(token, entityIds);
-                            var rcas = new List<Rca>();
-
-                            foreach (var id in storefrontIds)
-                            {
-                                var rca = await gqlApi.GetRcaAsync(token, id);
-                                
-                                if (rca != null) rcas.Add((Rca)rca);
-                            }
-
-                            rcas.ForEach(rca =>
-                            {
-                                Bot.PostRcaAsync(rca, MessageType.CONTRACT);
-                                var storefrontId = rca.ShopUrl.Substring(rca.ShopUrl.LastIndexOf('/') + 1);
-                                Database.AddStorefrontAsync(storefrontId);
-                            });
-
-                            entityIds.Reverse();
-                            if (rcas.Count > 0) Database.SetLastEntityIdAsync(entityIds[0]);
-                        }
+                        var lastIdIndex = entityIds.IndexOf(lastId);
+                        entityIds = entityIds.Slice(0, lastIdIndex);
                     }
+
+                    entityIds.Reverse();
+                    var storefrontIds = await gqlApi.GetStorefrontIdsAsync(token, entityIds);
+                    if (storefrontIds == null) throw new Exception("No storefrontIds fetched");
+                    
+                    var rcas = new List<Rca>();
+                    foreach (var id in storefrontIds)
+                    {
+                        var rca = await gqlApi.GetRcaAsync(token, id);
+                        if (rca != null) rcas.Add((Rca)rca);
+                    }
+
+                    rcas.ForEach(rca =>
+                    {
+                        Bot.PostRcaAsync(rca, MessageType.CONTRACT);
+                        var storefrontId = rca.ShopUrl.Substring(rca.ShopUrl.LastIndexOf('/') + 1);
+                        Database.AddStorefrontAsync(storefrontId);
+                    });
+
+                    entityIds.Reverse();
+                    if (rcas.Count > 0) Database.SetLastEntityIdAsync(entityIds[0]);
                 }
-                else
+                else if (lastId != entityIds[0])
                 {
-                    if (entityIds != null)
-                    {
-                        if (lastId != entityIds[0])
-                        {
-                            await Database.SetLastEntityIdAsync(entityIds[0]);
-                        }
-                    }
+                    await Database.SetLastEntityIdAsync(entityIds[0]);
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine($"Error getting contracts: {e.Message}");
             }
-        }
     }
 }
--- services/RcaService.cs
+++ services/RcaService.cs
@@ -1,5 +1,4 @@
 using ds_rca.bot;
-using ds_rca.config;
 using ds_rca.data.db;
 using ds_rca.data.entities;
 using ds_rca.data.remote.api;
@@ -16,58 +15,48 @@ public class RcaService(RedditApi api, RedditGqlApi gqlApi)
             {
                 var lastId = await Database.GetLastStorefrontIdAsync();
                 var storefrontIds = await api.GetStorefrontIdsAsync();
-                
+                if (storefrontIds != null) throw new Exception("No storefrontIds fetched");
+
                 if (lastId.Length > 0)
                 {
-                    if (storefrontIds != null)
+                    var token = await gqlApi.GetTokenAsync();
+                    if (token == null) throw new Exception("Token not generated");
+
+                    var isContainsLastId = storefrontIds.Contains(lastId);
+                    if (isContainsLastId)
                     {
-                        var token = await gqlApi.GetTokenAsync();
-
-                        if (token != null)
-                        {
-                            var isContainsLastId = storefrontIds.Contains(lastId);
-
-                            if (isContainsLastId)
-                            {
-                                var lastIdIndex = storefrontIds.IndexOf(lastId);
-                                storefrontIds = storefrontIds.Slice(0, lastIdIndex);
-                            }
-
-                            storefrontIds.Reverse();
-
-                            var rcas = new List<Rca>();
-                            
-                            foreach (var id in storefrontIds)
-                            {
-                                var rca = await gqlApi.GetRcaAsync(token, id);
-
-                                if (rca != null) rcas.Add((Rca)rca);
-                            }
-
-                            rcas.ForEach(rca =>
-                            {
-                                Bot.PostRcaAsync(rca, MessageType.RCA);
-                                var storefrontId = rca.ShopUrl.Substring(rca.ShopUrl.LastIndexOf('/') + 1);
-                                Database.DeleteStorefrontAsync(storefrontId);
-                            });
-
-                            storefrontIds.Reverse();
-                            if (rcas.Count > 0) await Database.SetLastStorefrontIdAsync(storefrontIds[0]);
-                        }
+                        var lastIdIndex = storefrontIds.IndexOf(lastId);
+                        storefrontIds = storefrontIds.Slice(0, lastIdIndex);
                     }
+
+                    storefrontIds.Reverse();
+                    var rcas = new List<Rca>();
+                    foreach (var id in storefrontIds)
+                    {
+                        var rca = await gqlApi.GetRcaAsync(token, id);
+                        if (rca != null) rcas.Add((Rca)rca);
+                    }
+
+                    rcas.ForEach(rca =>
+                    {
+                        Bot.PostRcaAsync(rca, MessageType.RCA);
+                        var storefrontId = rca.ShopUrl.Substring(rca.ShopUrl.LastIndexOf('/') + 1);
+                        Database.DeleteStorefrontAsync(storefrontId);
+                    });
+
+                    storefrontIds.Reverse();
+                    if (rcas.Count > 0) await Database.SetLastStorefrontIdAsync(storefrontIds[0]);
                 }
-                else
+                else if (lastId != storefrontIds[0])
                 {
-                    if (storefrontIds != null)
-                        if (lastId != storefrontIds[0])
-                            await Database.SetLastStorefrontIdAsync(storefrontIds[0]);
+                    await Database.SetLastStorefrontIdAsync(storefrontIds[0]);
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine($"Error getting rcas: {e.Message}");
             }
-            
+
             Thread.Sleep(1200);
         }
     }
