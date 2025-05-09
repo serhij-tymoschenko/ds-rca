c84690a733bd04dc18d1e94612270fd14bf35469
Serhii Tymoshenko <serhij.tymoschenko@gmail.com>
2025-05-09 09:44:56 +0200

    Moved LOG_CHANNEL_ID to environment

--- config/Config.cs
+++ config/Config.cs
@@ -3,8 +3,8 @@ namespace ds_rca.config;
 public class Config
 {
     public const int POLYSCAN_OFFSET = 40;
-    public const ulong LOG_CHANNEL_ID = 1370139109477060753;
 
+    public static ulong LOG_CHANNEL_ID = ulong.Parse(Environment.GetEnvironmentVariable("LOG_CHANNEL_ID"));
     public static string DS_API_KEY = Environment.GetEnvironmentVariable("DS_API_KEY");
     public static string POLYSCAN_API_KEY = Environment.GetEnvironmentVariable("POLYSCAN_API_KEY");
     public static string FIREBASE_PROJECT_ID = Environment.GetEnvironmentVariable("FIREBASE_PROJECT_ID");
